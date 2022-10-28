cbc1d263423d5c8cb666eb440995e8d1b27fb0f6
Luka Savkov <lukasavkov1@gmail.com>
2022-10-28 15:33:46 +0200

    prvo dodavanje

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezbe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Unesi broj: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+
+             bool includesInteger()
+            {
+                while (number > 0)
+                {
+                    if (number % 10 == 3) return true;
+                    number /= 10;
+                }
+                return false;
+            }
+
+            if (includesInteger())
+                Console.WriteLine("Ima broj 3");
+            else
+                Console.WriteLine("Nema broj 3");
+
+            Console.ReadKey();
+
+        }
+    }
+}
